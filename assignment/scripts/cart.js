fc742fa30de042c913a25d5076c1ba17b1f6243b
Zakariye Abdi <zak.abdi94@gmail.com>
2023-01-08 12:58:19 -0600

    added funtion cart git status

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,33 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// created cart and added item function
+let  cart = [];
+const maxItems = 5;
+
+function isFull() {
+if (cart.length < maxItems) {
+    return false; } else {
+        return true;
+    }
+
+}
+
+console.log ('cart')
+
+function addItem(item) {
+    if (isFull() ===true ) {
+console.log('your cart is full!');
+return false;
+
+    } else {
+        cart.push(item) // Adds a new item into the array
+    }
+
+
+
+
+
+
+}
